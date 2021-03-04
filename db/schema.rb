3c35b5ceddbedef7a4ca25e30b9b3476d75d1721
emmanuel-karanja <existentialkaranja@gmail.com>
2021-03-04 08:55:28 +0300

    added postgresql support with host as localhost

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_03_132710) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "facts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "fact"
