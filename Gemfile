15be98659866e29e4cb1d291b8c14bbd35f2f509
emmanuel-karanja <existentialkaranja@gmail.com>
2021-03-04 11:01:18 +0300

    replaced http with httpx

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'rack'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 
-gem 'http'
+gem 'httpx'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,16 +1,16 @@
-require 'http'
+require 'httpx'
 class Api::V1::PostsController < ApplicationController
     def index
         request_api('https://jsonplaceholder.typicode.com/todos')
         
         return nil if @response.status !=200
-        render json: @response.parse
+        render json: JSON.parse(@response.body)
     end
 
     private
 
     def request_api(url)
-        @response=HTTP.get(url)
+        @response=HTTPX.get(url) 
     end
 
 end
